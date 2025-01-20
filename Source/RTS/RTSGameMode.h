a51f09ffeba945373586ea181b4aba5311e950e6
takuryto2 <louisguichard761@gmail.com>
2025-01-20 14:25:38 +0100

    oui

---
+++ Source/RTS/RTSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RTSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARTSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARTSGameMode();
+};
+
+
+
