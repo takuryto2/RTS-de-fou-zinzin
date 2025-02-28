e0cfbce29f7127905267a11a36e9cc9b896293f0
takuryto2 <louisguichard761@gmail.com>
2025-02-28 10:41:53 +0100

    yes

--- Source/RTS/RTS.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RTS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTS, "RTS" );
-
-DEFINE_LOG_CATEGORY(LogRTS)
- 
